65286d4dbedf7eac97c23dcb548785c70b5b06a4
Zhenqing <lizhenqing.fudan@gmail.com>
2025-08-11 00:10:11 -0400

    Update the IronREPL setup

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -167,6 +167,7 @@ return {
             python = {
               command = { "python3" },
               format = require("iron.fts.common").bracketed_paste,
+              block_dividers = { "# %%", "#%%" },
             },
             julia = {
               command = { "julia" }
@@ -175,11 +176,21 @@ return {
           repl_open_cmd = require("iron.view").bottom(40),
         },
         keymaps = {
+              toggle_repl = "<space>rr", -- toggles the repl open and closed.
+          -- If repl_open_command is a table as above, then the following keymaps are
+          -- available
+          -- toggle_repl_with_cmd_1 = "<space>rv",
+          -- toggle_repl_with_cmd_2 = "<space>rh",
+          restart_repl = "<space>rR", -- calls `IronRestart` to restart the repl
           send_motion = "<space>sc",
           visual_send = "<space>sc",
           send_file = "<space>sf",
           send_line = "<space>sl",
+          send_paragraph = "<space>sp",
+          send_until_cursor = "<space>su",
           send_mark = "<space>sm",
+          send_code_block = "<space>sb",
+          send_code_block_and_move = "<space>sn",
           mark_motion = "<space>mc",
           mark_visual = "<space>mc",
           remove_mark = "<space>md",
